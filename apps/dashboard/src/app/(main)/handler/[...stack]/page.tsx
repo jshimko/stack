ef4f16efe45b8e85d305bc59dc24416424dd6996
Jeremy Shimko <jeremy.shimko@gmail.com>
2024-10-14 15:42:41 -0400

    remove privacy/terms if whitelable enabled

--- apps/dashboard/src/app/(main)/handler/[...stack]/page.tsx
+++ apps/dashboard/src/app/(main)/handler/[...stack]/page.tsx
@@ -1,12 +1,17 @@
+import { env } from "next-runtime-env";
 import { StackHandler } from "@stackframe/stack";
 import { stackServerApp } from "@/stack";
 import { StyledLink } from "@/components/link";
 
 export default function Handler(props: any) {
-  const extraInfo = <>
+  const extraInfo = env("NEXT_PUBLIC_WHITE_LABEL_ENABLED") !== "true" ? <>
     <p className="text-xs">By signing in, you agree to the</p>
-    <p className="text-xs"><StyledLink href="https://www.iubenda.com/privacy-policy/19290387/cookie-policy">Terms of Service</StyledLink> and <StyledLink href="https://www.iubenda.com/privacy-policy/19290387">Privacy Policy</StyledLink></p>
-  </>;
+    <p className="text-xs">
+      <StyledLink href="https://www.iubenda.com/privacy-policy/19290387/cookie-policy">Terms of Service</StyledLink> and
+      <StyledLink href="https://www.iubenda.com/privacy-policy/19290387">Privacy Policy</StyledLink>
+    </p>
+  </> : null;
+
   return <StackHandler
     fullPage {...props}
     app={stackServerApp}
